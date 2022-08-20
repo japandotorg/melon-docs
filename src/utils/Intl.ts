cc91fbfea2d2beeb1f2a6b3318813526f2ce0e6a
Lemon Rose <yash.kul69@gmail.com>
2022-08-21 02:56:30 +0530

    intl

---
+++ src/utils/Intl.ts
@@ -0,0 +1,7 @@
+const IntlFormatter = new Intl.NumberFormat("en-US", {
+    maximumFractionDigits: 2
+});
+
+export function formatNumber(number: number): string {
+    return IntlFormatter.format(number);
+};
