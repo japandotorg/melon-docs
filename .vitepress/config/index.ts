05a41445370e7521133d98e908936681c31023a0
Lemon Rose <yash.kul69@gmail.com>
2022-08-22 00:39:49 +0530

    better index

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -1,9 +1,15 @@
 import { UserConfig } from "vitepress";
 import { DefaultTheme }  from "@windicss/vitepress-theme/config";
 
+import head from "./head";
+import themeConfig from "./theme";
+import { metaData } from "./constants";
+
 const config: UserConfig<DefaultTheme.Config> = {
-    title: "Windi CSS",
-    description: "description",
+    title: "Melon Docs",
+    description: metaData.description,
+    head,
+    themeConfig,
 }
 
 export default config;
