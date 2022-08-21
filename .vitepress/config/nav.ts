e842df32ef7242e3f3612fc09ecbdb01e06bff08
Lemon Rose <yash.kul69@gmail.com>
2022-08-22 00:34:54 +0530

    nav

---
+++ .vitepress/config/nav.ts
@@ -0,0 +1,10 @@
+import type { DefaultTheme } from "@windicss/vitepress-theme/config";
+
+export const nav: DefaultTheme.Config["nav"] = [
+    {
+        text: "Docs",
+        items: [
+            { text: "Getting Started", link: "/docs/getting-started" },
+        ]
+    }
+]
