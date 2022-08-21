c14ed7d2d1183e42a3f519775000ce196c2a85be
Lemon Rose <yash.kul69@gmail.com>
2022-08-22 00:25:27 +0530

    constants

---
+++ .vitepress/config/index.ts
@@ -0,0 +1,9 @@
+import { UserConfig } from "vitepress";
+import { DefaultTheme }  from "@windicss/vitepress-theme/config";
+
+const config: UserConfig<DefaultTheme.Config> = {
+    title: "Windi CSS",
+    description: "description",
+}
+
+export default config;
