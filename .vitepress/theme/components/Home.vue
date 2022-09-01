858e0bab011b7de247d8ae7473c4bd59bdd6d5be
Lemon Rose <yash.kul69@gmail.com>
2022-09-01 17:32:20 +0530

    home

---
+++ .vitepress/theme/components/Home.vue
@@ -0,0 +1,274 @@
+<script setup lang="ts">
+    import { onMounted } from "vue";
+    import { load } from "./sponsors";
+    
+    onMounted(async () => {
+        await load()
+    })
+</script>
+
+<template>
+    <section id="hero">
+        <h1 class="tagline">
+            MELON
+            <span class="accent">DOCS</span>
+            <br />
+        </h1>
+        <p class="description">
+            A Multipurpose Discord Bot
+        </p>
+        <p class="actions">
+            <vue-mastery-modal />
+            <a class="get-started" href="/docs/intro.md">
+                DOCS
+                <svg 
+                    class="icon" 
+                    xmlns="https://www.w2/org/2000/svg" 
+                    width="10" 
+                    height="10" 
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" 
+                    />
+                </svg>
+            </a>
+            <a class="invite" href="">Invite</a>
+        </p>
+    </section>
+
+    <section id="highlights" class="vt-box-container">
+        <div class="vt-box">
+            <h2>Giveaways</h2>
+            <p>
+                Melon comes with a powerful giveaways system with tons of customisable features.
+            </p>
+        </div>
+        <div class="vt-box">
+            <h2>Donation Logging</h2>
+            <p>
+                Melon comes with a powerful donation logging system with multiple categories and
+                tons of customisable features.
+            </p>
+        </div>
+        <div class="vt-box">
+            <h2>Custom Commands</h2>
+            <p>
+                Melon comes with the ability to create powerful custom commands with the usage of
+                tagscript engine.
+            </p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+    section {
+        padding: 42px 32px;
+    }
+    
+    #hero {
+        padding: 96px 32px;
+        text-align: center;
+    }
+    
+    .tagline {
+        font-size: 76px;
+        line-height: 1.25;
+        font-weight: 900;
+        letter-spacing: -1.5px;
+        max-width: 960px;
+        margin: 0px auto;
+    }
+    
+    html:not(.dark) .accent,
+    .dark .tagline {
+        background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+    
+    .description {
+        max-width: 960px;
+        line-height: 1.5;
+        color: var(--vt-c-text-2);
+        transition: color 0.5s;
+        font-size: 22px;
+        margin: 24px auto 40px;
+    }
+    
+    .actions a {
+        font-size: 16px;
+        display: inline-block;
+        background-color: var(--vt-c-bg-mute);
+        padding: 8px 18px;
+        font-weight: 500;
+        border-radius: 8px;
+        transition: background-color 0.5s, color 0.5s;
+    }
+    
+    .actions .get-started {
+        margin-right: 18px;
+    }
+    
+    .actions .icon {
+        display: inline;
+        position: relative;
+        top: -1px;
+        margin-left: 2px;
+        fill: currentColor;
+        transition: transform 0.2s;
+    }
+    
+    .actions .get-started:hover {
+        transition-duration: 0.2s;
+    }
+    
+    .actions .get-started:hover .icon {
+        transform: translateX(2px);
+    }
+    
+    .actions .get-started,
+    .actions .setup {
+        color: var(--vt-c-text-code);
+    }
+    
+    .actions .get-started:hover,
+    .actions .setup:hover {
+        background-color: var(--vt-c-gray-light-4);
+        transition-duration: 0.2s;
+    }
+    
+    .dark .actions .get-started:hover,
+    .dark .actions .setup:hover {
+        background-color: var(--vt-c-gray-dark-3);
+    }
+    
+    #hero {
+        padding: 96px 32px;
+        text-align: center;
+    }
+    
+    .tagline {
+        font-size: 76px;
+        line-height: 1.25;
+        font-weight: 900;
+        letter-spacing: -1.5px;
+        max-width: 960px;
+        margin: 0px auto;
+    }
+    
+    html:not(.dark) .accent,
+    .dark .tagline {
+        background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+    
+    .description {
+        max-width: 960px;
+        line-height: 1.5;
+        color: var(--vt-c-text-2);
+        transition: color 0.5s;
+        font-size: 22px;
+        margin: 24px auto 40px;
+    }
+    
+    .actions a {
+        font-size: 16px;
+        display: inline-block;
+        background-color: var(--vt-c-bg-mute);
+        padding: 8px 18px;
+        font-weight: 500;
+        border-radius: 8px;
+        transition: background-color 0.5s, color 0.5s;
+    }
+    
+    .actions .get-started {
+        margin-right: 18px;
+    }
+    
+    .actions .icon {
+        display: inline;
+        position: relative;
+        top: -1px;
+        margin-left: 2px;
+        fill: currentColor;
+        transition: transform 0.2s;
+    }
+    
+    .actions .get-started:hover {
+        transition-duration: 0.2s;
+    }
+    
+    .actions .get-started:hover .icon {
+        transform: translateX(2px);
+    }
+    
+    .actions .get-started,
+    .actions .setup {
+        color: var(--vt-c-text-code);
+    }
+    
+    .actions .get-started:hover,
+    .actions .setup:hover {
+        background-color: var(--vt-c-gray-light-4);
+        transition-duration: 0.2s;
+    }
+    
+    .dark .actions .get-started:hover,
+    .dark .actions .setup:hover {
+        background-color: var(--vt-c-gray-dark-3);
+    }
+    
+    @media (max-width: 960px) {
+        .tagline {
+            font-size: 64px;
+            letter-spacing: -0.5px;
+        }
+    
+        .description {
+            font-size: 18px;
+            margin-bottom: 48px;
+        }
+    }
+    
+    @media (max-width: 768px) {
+        .tagline {
+            font-size: 48px;
+            letter-spacing: -0.5px;
+        }
+    }
+    
+    @media (max-width: 576px) {
+        #hero {
+            padding: 64px 32px;
+        }
+    
+        .description {
+            font-size: 16px;
+            margin: 18px 0 30px;
+        }
+    
+        #highlights h3 {
+            margin-bottom: 0.6em;
+        }
+    
+        #highlights .vt-box {
+            padding: 20px 36px;
+        }
+    
+        .actions a {
+            margin: 0.5em 0;
+        }
+    }
+    
+    @media (max-width: 370px) {
+        .tagline {
+            font-size: 36px;
+        }
+    }
+
+</style>
