e35a888fd9d7c0c0eca1ce421c761100d70cb3a2
Lemon Rose <yash.kul69@gmail.com>
2022-12-03 21:02:34 +0530

    config

---
+++ migrations/config.php
@@ -0,0 +1,17 @@
+<?php defined('_DCHECK') or exit('Restricted Access');
+
+    define('VERSION', '0.0.0.dev666');
+    define('DEV_MODE', 'OFF');
+
+    define('SITE_URL', 'https://guide.melonbot.io/');
+
+    define('SRV_NAME', 'MELON DOCS');
+
+    define('CACHE_LIFETIME', '0');
+
+    define("MODULE_DISCORD" , "ON");
+
+    define("DISCORD_SERVER_ID" , "926230917641560114");
+
+    define('DISCORD_INVITE_LINK', 'https://discord.gg/mXfYuMy92r');
+?>
